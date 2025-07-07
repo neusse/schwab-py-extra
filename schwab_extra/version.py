9f6525081755f4de416fb8aa3fecfed6e5374676
Neusse <neusse@gmail.com>
2025-07-06 18:05:32 -0700

    pyproject.tpml upgrade

--- schwab_extra/version.py
+++ schwab_extra/version.py
@@ -1,1 +1,5 @@
-version = '0.0.31'
+"""Version information for schwab-py-extra."""
+
+__version__ = "0.1.0"
+version = __version__  # Backward compatibility
+__version_info__ = tuple(int(i) for i in __version__.split("."))
